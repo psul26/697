f2b9a7f5a11bffea9d0d3d4315a7b99aa6f384d0
Patrick Sullivan <sully2693@gmail.com>
2016-03-01 00:43:35 -0500

    solution

--- partb.sh
+++ partb.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-for i in {1..20}
-do
+for i in {1..1000}
+do	
+	echo "new line"
 	echo $i
-	python CNFGen.py ex4.bench ex4.state $i | sed -n 1p 
+	python CNFGen.py ex3.bench ex3.state $i 
+	
+	
 	picosat out | sed -n 1p
+	#picosat -v out | tail >> picoTail.txt
 done
