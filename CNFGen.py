bd8086c8c8df2b42ea89adf2dccc39a41f686eb5
Patrick Sullivan <sully2693@gmail.com>
2016-02-29 20:47:43 -0500

    finished cnf generator

--- CNFGen.py
+++ CNFGen.py
@@ -1,17 +1,19 @@
 import sys
-unrolls = input("Please enter the # of unrolls")#unroll 1 is the init
+unrolls = int(sys.argv[3])
 d = {} #general dictionary of all info if needed
 DFF = {} #DFF output cur state indexs to name of input next state
 nodeToNumber = {}#node name indexs to gate number
 gateNumbers ={} #keeps track of number of gates per type
 ands = {} #output name indexs to input1 and input2 names resp
 nots = {} #output name indexs to input name
-
-
+targetState = ""
+listDFF = []
 def numberOfClauses():
 	return (gateNumbers["DFF"]+(3*gateNumbers["AND"]+2*gateNumbers["NOT"])*unrolls+gateNumbers["DFF"]+2*gateNumbers["DFF"]*(unrolls-1))
 def numberOfVariables():
 	return len(nodeToNumber)*unrolls
+
+
 
 reader = open(sys.argv[1],"r")
 count = 1
@@ -51,12 +53,20 @@ for line in reader.readlines():
 			d[line[0]] = line[1],line[2],0
 			nodeToNumber[line[0]] = count
 			gateNumbers["DFF"] = gateNumbers["DFF"] + 1
+			listDFF.append(str(line[0]))
 			count = count + 1
 			
-
-
 reader.close()
-writer = open("CNFGeneratedFile","w")
+if len(sys.argv) == 4:
+	reader = open(sys.argv[2],"r")
+	for line in reader.readlines():
+		
+		if not line == '\n':	
+			targetState = str(line).strip()
+	
+reader.close()
+
+writer = open("out","w")
 
 writing = True
 numberOfNodes = len(nodeToNumber)
@@ -91,11 +101,21 @@ while writing:
 		#now time for the linking
 		writer.write("c linking begins \n")
 		if not i==0:
-			for j in DFF:
+			for j in listDFF:
 				writer.write("-"+str(nodeToNumber[DFF[j]]+(i-1)*numberOfNodes) +" "+ str(nodeToNumber[j]+i*numberOfNodes)+" 0\n")
 				writer.write(str(nodeToNumber[DFF[j]]+(i-1)*numberOfNodes) +" -"+ str(nodeToNumber[j]+i*numberOfNodes)+" 0\n")
 		writer.write("c linking ends \n")
 		writer.write("c loop: "+str(i)+" ends \n")
 	writing =False
-print (numberOfVariables(),numberOfClauses())
+writer.write("c \n")
+writer.write("c begin target states \n")
+num = 0
+
+for h in listDFF:
+	if targetState[num] == "0":
+		writer.write("-"+str(nodeToNumber[DFF[h]]+i*numberOfNodes)+" 0\n")
+	if targetState[num] == "1":
+		writer.write(str(nodeToNumber[DFF[h]]+i*numberOfNodes)+" 0\n")
+	num = num + 1
+
 
